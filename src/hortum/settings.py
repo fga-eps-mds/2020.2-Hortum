49b2e5880e01e0d17e9a268846323df3f886fb87
Joao-Moura <jaummoura15@gmail.com>
2021-02-24 12:53:13 +0000

    #21 Correção do app assets dentro das settings

--- src/hortum/settings.py
+++ src/hortum/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'assets.apps.AssetsConfig',
+    'hortum.assets',
     'corsheaders',
 ]
 
