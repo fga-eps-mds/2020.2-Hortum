f77c1dca520cfae31cdffb6b29faf2ce4977fcf6
Joao-Moura <jaummoura15@gmail.com>
2021-03-16 16:53:26 +0000

    #81 Adicionando acesso de tokens pelo Simple JWT
    Co-authored-by: brenno-silva <brenno.silva037@gmail.com>
    Co-authored-by: CarlosFiuza <carlosfiuza1234@gmail.com>

--- src/hortum/urls.py
+++ src/hortum/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 urlpatterns = [
+    path('api/token/', TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
     path('signup/', include('hortum.users.urls')),
 ]
