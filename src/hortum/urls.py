cf3211532690a5f5d5311c8218a8b6d21183b4d8
Joao-Moura <jaummoura15@gmail.com>
2021-03-18 15:36:51 +0000

    #81 Trocando rota api/token/ para login/

--- src/hortum/urls.py
+++ src/hortum/urls.py
@@ -4,8 +4,8 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view()),
-    path('api/token/refresh/', TokenRefreshView.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
+    path('login/refresh/', TokenRefreshView.as_view()),
     path('admin/', admin.site.urls),
     path('signup/', include('hortum.users.urls')),
 ]
