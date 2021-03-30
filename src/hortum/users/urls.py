c380bba6c4e90b9d0b303572984722b302168397
LucasBraunX <78307547+LucasBraunX@users.noreply.github.com>
2021-03-30 17:50:58 -0300

    #130 ajustando rotas de listagem de produtor e anúncio
    Co-authored-by: matheuscvp <matheuscvp12@gmail.com>

--- src/hortum/users/urls.py
+++ src/hortum/users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 
 from rest_framework import routers
+from ..productor.urls import routerRegister as productorRegister
+from ..customer.urls import routerRegister as customerRegister
 
 from . import viewsets
 
@@ -8,6 +10,6 @@ router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'confirm', viewsets.UserListRetrieveAPIView, basename='user')
 
 urlpatterns = [
-    path('customer/', include('hortum.customer.urls')),
-    path('productor/', include('hortum.productor.urls')),
+    path('customer/', include(customerRegister.urls)),
+    path('productor/', include(productorRegister.urls)),
 ] + router.urls
