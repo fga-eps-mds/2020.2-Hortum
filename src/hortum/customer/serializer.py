3f0ccb50d723c709b1134f31e4033223f0dabc49
Joao-Moura <jaummoura15@gmail.com>
2021-04-16 20:41:10 +0000

    #104 Adicionando nova viewset e serializer para adição de anúncios aos favoritos

--- src/hortum/customer/serializer.py
+++ src/hortum/customer/serializer.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
 from .models import Customer
+from ..productor.models import Productor
+from ..announcement.models import Announcement
 from ..users.models import User
 
 from ..users.serializer import UserSerializer
@@ -10,7 +12,7 @@ from ..productor.serializer import ProductorSerializer
 
 class CustomerSerializer(serializers.ModelSerializer):
     user = UserSerializer(required=True)
-    idPicture = PictureSerializer(many=False, read_only=True)
+    idPicture = PictureSerializer(read_only=True)
     idAnunFav = AnnouncementCreateSerializer(many=True, read_only=True)
     idProdFav = ProductorSerializer(many=True, read_only=True)
 
@@ -22,3 +24,21 @@ class CustomerSerializer(serializers.ModelSerializer):
         user_data = validated_data.pop('user')
         user = User.objects.create_user(**user_data)
         return Customer.objects.create(user=user, **validated_data)
+
+class CustomerAddAnnouncementSerializer(serializers.ModelSerializer):
+    announcementName = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = Customer
+        fields = ['email', 'announcementName']
+
+    def validate_email(self, email):
+        if not Productor.objects.filter(user__email=email).exists():
+            raise serializers.ValidationError('Email de produtor inexistente')
+        return email
+
+    def validate_announcementName(self, name):
+        if not Announcement.objects.filter(name=name).exists():
+            raise serializers.ValidationError('Nome de anúncio inexistente')
+        return name
--- src/hortum/customer/viewsets.py
+++ src/hortum/customer/viewsets.py
@@ -1,10 +1,37 @@
-from .serializer import CustomerSerializer
-from .models import Customer
-from rest_framework.viewsets import GenericViewSet
-from rest_framework import mixins
-from rest_framework import permissions
+from . import serializer
+from ..announcement.serializer import AnnouncementListSerializer
 
-class CustomerRegistrationAPIView (GenericViewSet, mixins.CreateModelMixin):
+from .models import Customer
+from ..announcement.models import Announcement
+from ..productor.models import Productor
+
+from rest_framework.viewsets import GenericViewSet
+from rest_framework import mixins, permissions
+from rest_framework.response import Response
+
+class CustomerRegistrationAPIView (GenericViewSet, mixins.CreateModelMixin, mixins.ListModelMixin):
+	'''
+	EndPoint para registro de User's
+	'''
 	permission_classes = (permissions.AllowAny,)
-	serializer_class = CustomerSerializer
+	serializer_class = serializer.CustomerSerializer
 	queryset = Customer.objects.all()
+
+class AddFavoritesAnnouncementsAPIView (GenericViewSet, mixins.UpdateModelMixin):
+	'''
+	EndPoint para adição/remoção de anúncios da lista de favoritos
+	'''
+	permission_classes = (permissions.IsAuthenticated,)
+	serializer_class = serializer.CustomerAddAnnouncementSerializer
+
+	def get_object(self):
+		return Customer.objects.get(user__email=self.request.user)
+
+	def update(self, request, *args, **kwargs):
+		instance = self.get_object()
+		serializer = self.get_serializer(data=request.data)
+		serializer.is_valid(raise_exception=True)
+		anun = Announcement.objects.get(idProductor=Productor.objects.get(user__email=serializer.data.get('email')), name=serializer.data.get('announcementName'))
+		instance.idAnunFav.remove(anun) if instance.idAnunFav.filter(pk=anun.pk).exists() else instance.idAnunFav.add(anun)
+
+		return Response('Annúncio atualizado com sucesso', status=200)
