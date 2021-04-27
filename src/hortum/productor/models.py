21bdee04533c8d2d0922cb9bcb483d7e0678861a
VitorLamego <vitorlamegoo@gmail.com>
2021-04-27 18:45:46 -0300

    #183 Adicionando variável de imagem ao Productor
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>
    Co-authored-by: matheuscvp <matheuscvp12@gmail.com>

--- src/hortum/productor/models.py
+++ src/hortum/productor/models.py
@@ -1,10 +1,8 @@
 from django.db import models
-from hortum.picture.models import Picture
 from hortum.users.models import User
 
 class Productor(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    idPicture = models.OneToOneField(Picture, on_delete=models.CASCADE, null=True)
 
 class Localization(models.Model):
     adress = models.CharField(max_length=100)
