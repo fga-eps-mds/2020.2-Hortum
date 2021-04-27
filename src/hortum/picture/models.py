5eb752451e30c388c50381dcec2580affa5983fe
VitorLamego <vitorlamegoo@gmail.com>
2021-04-27 18:40:51 -0300

    #183 Removendo entidade Picture
    Co-authored-by: Joao-Moura <jaummoura15@gmail.com>
    Co-authored-by: matheuscvp <matheuscvp12@gmail.com>

--- src/hortum/picture/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class Picture(models.Model):
-    name = models.CharField(max_length=20)
-    picture = models.ImageField(upload_to='uploaded')
--- src/hortum/picture/serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Picture
-
-class PictureSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Picture
-        fields = ['name', 'picture']
