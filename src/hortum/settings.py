ad0804b021187f1a6a420a42404a2f2284c1ac03
LucasBraunX <78307547+LucasBraunX@users.noreply.github.com>
2021-04-28 19:07:52 -0300

    #163 alterando configurações para permitir envio de email
    Co-authored-by: vital14 <limavictor49@gmail.com>
    Co-authored-by: brenno-silva <brenno.silva037@gmail.com>

--- src/hortum/settings.py
+++ src/hortum/settings.py
@@ -167,3 +167,9 @@ SIMPLE_JWT = {
 }
 
 django_heroku.settings(locals())
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
