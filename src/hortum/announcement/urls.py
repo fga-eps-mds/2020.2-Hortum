435dfbd732db20e800acd18218b9f9a5b8c7286a
matheuscvp <matheuscvp12@gmail.com>
2021-05-04 01:11:27 -0300

    #208 adicionando url de requisição de anuncios com determinado tipo/categoria

--- src/hortum/announcement/urls.py
+++ src/hortum/announcement/urls.py
@@ -7,6 +7,7 @@ router.register(r'create', viewsets.AnnouncementRegistrationAPIView, basename='c
 router.register(r'update', viewsets.AnnouncementDeleteUpdateAPIView, basename='deleteUpdateAnnoun')
 router.register(r'list(:?/(?P<announcementName>.+))?', viewsets.AnnouncementListAPIView, basename='listAnnoun')
 router.register(r'retrieve/(?P<encoded_email>.+)', viewsets.AnnouncementProductorListAPIView, basename='retrieveAnnounProd')
+router.register(r'category/(?P<type_of_product>.+)', viewsets.AnnouncementListCategoryView, basename='ListAnnouCategory')
 
 urlpatterns = [
 ] + router.urls
