2290972f57b226bb38ae632bedd9848667448a35
Joao-Moura <jaummoura15@gmail.com>
2021-05-08 16:21:14 +0000

    #221 Separando o validator em validators globais e locais

---
+++ src/hortum/users/validators.py
@@ -0,0 +1,24 @@
+from rest_framework.utils.representation import smart_repr
+from rest_framework.exceptions import ValidationError
+
+class PasswordValidator:
+    '''
+    Validação que checa se a senha passada está correta.
+    Implementação baseada no `UniqueValidator`.
+    '''
+    message = 'Senha incorreta.'
+    requires_context = True
+
+    def __init__(self, password='password', message=None):
+        self.password = password
+        self.message = message or self.message
+
+    def __call__(self, value, serializer):
+        if not serializer.context['request'].user.check_password(value):
+            raise ValidationError(self.message)
+
+    def __repr__(self):
+        return '<%s(password=%s)>' % (
+            self.__class__.__name__,
+            smart_repr(self.password)
+        )
--- src/hortum/validators.py
+++ src/hortum/validators.py
@@ -17,6 +17,11 @@ def qs_filter(queryset, **kwargs):
     except (TypeError, ValueError, DataError):
         return queryset.none()
 
+# Implementação que filtra dependendo do nome do campo
+def filter_queryset(value, queryset, field_name, lookup):
+    filter_kwargs = {'%s__%s' % (field_name, lookup): value}
+    return qs_filter(queryset, **filter_kwargs)
+
 class UniqueValidator:
     '''
     Validação que checa se um campo é único.
@@ -31,19 +36,13 @@ class UniqueValidator:
         self.message = message or self.message
         self.lookup = lookup
 
-    def filter_queryset(self, value, queryset, field_name):
-        '''
-        Queryset que filtra todos values iguais
-        '''
-        filter_kwargs = {'%s__%s' % (field_name, self.lookup): value}
-        return qs_filter(queryset, **filter_kwargs)
-
     def __call__(self, value, serializer_field):
         # Determinando o nome do campo
         field_name = '__'.join(serializer_field.source_attrs)
 
+        # Filtragem da queryset
         queryset = self.queryset or serializer_field.context['view'].get_queryset()
-        queryset = self.filter_queryset(value, queryset, field_name)
+        queryset = filter_queryset(value, queryset, field_name, self.lookup)
         if qs_exists(queryset):
             raise ValidationError(self.message, code='unique')
 
@@ -61,26 +60,20 @@ class ExistingValidator:
     message = '{value} inexistente.'
     requires_context = True
 
-    def __init__(self, queryset, lookup='exact'):
+    def __init__(self, queryset=None, lookup='exact'):
         self.queryset = queryset
         self.lookup = lookup
-
-    def filter_queryset(self, value, queryset, field_name):
-        '''
-        Queryset que filtra todos values iguais
-        '''
-        filter_kwargs = {'%s__%s' % (field_name, self.lookup): value}
-        return qs_filter(queryset, **filter_kwargs)
 
     def __call__(self, value, serializer_field):
         # Determinando o nome do campo
         field_name = '__'.join(serializer_field.source_attrs)
 
         # Montando a mensagem baseada no campo
-        self.message = self.message.format(value=' '.join(serializer_field.source_attrs).capitalize())
+        self.message = self.message.format(value=serializer_field.source_attrs[-1].capitalize())
 
-        queryset = self.queryset
-        queryset = self.filter_queryset(value, queryset, field_name)
+        # Filtragem da queryset
+        queryset = self.queryset or serializer_field.context['view'].get_queryset()
+        queryset = filter_queryset(value, queryset, field_name, self.lookup)
         if not qs_exists(queryset):
             raise ValidationError(self.message, code='unique')
 
@@ -88,26 +81,4 @@ class ExistingValidator:
         return '<%s(queryset=%s)>' % (
             self.__class__.__name__,
             smart_repr(self.queryset)
-        )
-
-class PasswordValidator:
-    '''
-    Validação que checa se a senha passada está correta.
-    Implementação baseada no `UniqueValidator`.
-    '''
-    message = 'Senha incorreta.'
-    requires_context = True
-
-    def __init__(self, password='password', message=None):
-        self.password = password
-        self.message = message or self.message
-
-    def __call__(self, value, serializer):
-        if not serializer.context['request'].user.check_password(value):
-            raise ValidationError(self.message)
-
-    def __repr__(self):
-        return '<%s(password=%s)>' % (
-            self.__class__.__name__,
-            smart_repr(self.password)
         )
