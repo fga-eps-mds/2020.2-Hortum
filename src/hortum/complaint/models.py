ea5689edf8209fddb134b28e8ae901d00cf4226b
LucasBraunX <78307547+LucasBraunX@users.noreply.github.com>
2021-05-10 22:37:10 -0300

    #220 Alterando limite de tamanho de campos de nome

--- src/hortum/complaint/models.py
+++ src/hortum/complaint/models.py
@@ -5,7 +5,7 @@ class Complaint(models.Model):
     def upload_image(instance, filename):
         return f"{instance.author}-{filename}"
 
-    author = models.CharField(max_length=100)
+    author = models.CharField(max_length=30)
     description = models.CharField(max_length=450)
     idProductor = models.ForeignKey(Productor, on_delete=models.CASCADE, related_name='reclamations')
     image = models.ImageField(upload_to=upload_image, null=True)
